c8fd82a32df8073692f3788b7c9259e8906d5182
山下翔平 <yamashitashouhei@yamashitashouheinoMacBook-puro.local>
2018-12-15 23:59:17 +0900

    add friend title

--- src/screens/AddFriend.js
+++ src/screens/AddFriend.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, TouchableHighlight} from 'react-native';
 
 export default class AddFriend extends Component {
+  static navigationOptions = ({ navigation }) => {
+    let headerTitle = (<Text style={styles.addTitle}>友達を追加</Text>);
+    let headerTitleStyle: {alignSelf:"center"};
+    return {headerTitle, headerTitleStyle}
+  }
   render() {
     return(
       <View style={styles.container}>
