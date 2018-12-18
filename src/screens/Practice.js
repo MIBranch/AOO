099e0fbe19d264201218602b9791097f0f3aa427
山下翔平 <yamashitashouhei@yamashitashouheinoMacBook-puro.local>
2018-12-19 00:43:21 +0900

    popUpEx

--- src/screens/Practice.js
+++ src/screens/Practice.js
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Alert, Button, Text, View } from 'react-native';
 
 export default class Practice extends Component {
-
-  constructor(props) {
-    super(props);
-    this.title = 'Mooonsaan'
-    this.message = '1weekWaite';
-  }
-
   render() {
     return (
       <View style={styles.base}>
-        <View style={styles.small} />
-        <View style={styles.middle} />
-        <View style={styles.large} />
+        <Text style={styles.title}>Hello!!</Text>
+        <Button title="Click" onPress={this.doAction} />
       </View>
     );
+  }
+
+  doAction = ()=>{
+    Alert.alert('you clicked!!');
   }
 }
 
@@ -25,28 +21,11 @@ const styles = StyleSheet.create({
   base: {
     padding: 30
   },
-  small: {
-    width: 100,
-    height: 100,
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderColor: 'black',
-    backgroundColor: '#00aa00',
-  },
-  middle: {
-    width: 200,
-    height: 200,
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderColor: 'black',
-    backgroundColor: '#66ff66',
-  },
-  large: {
-    width: 300,
-    height: 300,
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderColor: 'black',
-    backgroundColor: '#ccffcc',
+  title: {
+    padding: 10,
+    color: 'red',
+    textAlign: 'center',
+    fontSize: 60,
+    fontWeight: 'bold'
   }
 });
