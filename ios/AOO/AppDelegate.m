07b0d8328a49d69ed92382495fcbd9b2a92b402b
山下翔平 <yamashitashouhei@yamashitashouheinoMacBook-puro.local>
2019-01-06 16:51:45 +0900

    #5 firebase for ios

--- ios/AOO/AppDelegate.m
+++ ios/AOO/AppDelegate.m
@@ -5,7 +5,9 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+
 #import "AppDelegate.h"
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -14,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
---
+++ ios/Pods/Target Support Files/Pods-AOOTests/Pods-AOOTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AOOTests : NSObject
+@end
+@implementation PodsDummy_Pods_AOOTests
+@end
